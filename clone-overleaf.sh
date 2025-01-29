c8696a337efbc8dce6db7383e2f671eb4902f99e
himarc <marc.himmelberger@inf.ethz.ch>
2025-01-29 17:21:14 +0100

    Fix divergent latex folder

--- clone-overleaf.sh
+++ clone-overleaf.sh
@@ -5,9 +5,13 @@ if ! git remote | grep -q latex; then
     git remote add -f latex https://git:olp_6CYA7UJ8bd3GebksBVNGiBJMKTXcDq3k144k@git.overleaf.com/6780e2bde2975c5d641bb05d
 fi
 
-# Preserve state
-push1=$(git stash push --keep-index --include-untracked -m "unstaged")
-push2=$(git stash push -m "staged")
+# Preserve state, but not latex/ folder
+push1=$(git stash push --keep-index --include-untracked -m "unstaged" -- ':!latex/')
+push2=$(git stash push -m "staged" -- ':!latex/')
+
+# Clean and check out last latex/ state
+rm -rf latex/
+git reset --hard
 
 # Clone Overleaf
 git pull --no-edit --squash -s subtree latex master
