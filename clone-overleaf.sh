56d6334efe7b97679f5709ddaa5315a2b9c7fa7f
himarc <marc.himmelberger@inf.ethz.ch>
2025-01-29 17:17:48 +0100

    Start on Encoding for Classic McEliece

--- clone-overleaf.sh
+++ clone-overleaf.sh
@@ -10,8 +10,8 @@ push1=$(git stash push --keep-index --include-untracked -m "unstaged")
 push2=$(git stash push -m "staged")
 
 # Clone Overleaf
-git pull --no-edit -s subtree latex master
-git commit --amend -m "Merge Overleaf"
+git pull --no-edit --squash -s subtree latex master
+git commit -m "Merge Overleaf"
 
 # Restore state
 if [ "No local changes to save" != "$push1" ]; then
