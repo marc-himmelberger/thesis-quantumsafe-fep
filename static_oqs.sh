f28d0a02461cd2197079905938dfb54ed847615a
Marc Himmelberger <marc.himmelberger@inf.ethz.ch>
2025-03-21 11:57:12 +0100

    Removed OpenSSL dependency, changed to static compilation of liboqs

--- static_oqs.sh
+++ static_oqs.sh
@@ -9,12 +9,12 @@ fi
 # WSL: Linux Marc-PC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux
 
 sudo apt update
-sudo apt install -y astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind
-#brew install cmake ninja openssl@3 wget doxygen graphviz astyle valgrind
+sudo apt install -y astyle cmake gcc ninja-build python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind musl musl-tools
+#brew install cmake ninja wget doxygen graphviz astyle valgrind
 #pip3 install pytest pytest-xdist pyyaml
 
 git clone --depth=1 https://github.com/open-quantum-safe/liboqs
-cmake -S liboqs -B liboqs/build -DCMAKE_INSTALL_PREFIX="$PWD/liboqs" -DBUILD_SHARED_LIBS=ON
+cmake -S liboqs -B liboqs/build -DCMAKE_INSTALL_PREFIX="$PWD/liboqs" -DOQS_USE_OPENSSL=OFF
 cmake --build liboqs/build --parallel 8
 cmake --build liboqs/build --target install
 
@@ -28,14 +28,18 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_path
 git clone --depth=1 https://github.com/open-quantum-safe/liboqs-go
 
 # Yes, we also need to put the paths into this file
-pc_file=$PWD/liboqs-go/.config/liboqs-go.pc
+pc_file=$PWD/liboqs-go/.config-static/liboqs-go.pc
 sed -i "s|/usr/local/include|$include_path|g" "$pc_file"
 sed -i "s|/usr/local/lib|$lib_path|g" "$pc_file"
+sed -i "s|-lcrypto ||g" "$pc_file"
 
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/liboqs-go/.config
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/liboqs-go/.config-static
 
 cd liboqs-go
 go test -v ./oqstests
-# TODO: Switch to static compilation as per https://github.com/open-quantum-safe/liboqs-go?tab=readme-ov-file
-# -> modify DBUILD_SHARED_LIBS, disable OpenSSL, remove OpenSSL dependency
-# TODO: Make sure lyrebird go finds this package after compilation
+
+# register local package as replacement via
+cd ..
+cd lyrebird
+go mod edit -replace github.com/open-quantum-safe/liboqs-go=../liboqs-go
+go mod tidy
