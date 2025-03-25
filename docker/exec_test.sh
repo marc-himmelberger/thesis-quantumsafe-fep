d93f93d14c5825002891d1394cb407ef36e6d900
Marc Himmelberger <marc.himmelberger@inf.ethz.ch>
2025-03-25 10:48:44 +0100

    Client tcpdump and log files

--- docker/exec_test.sh
+++ docker/exec_test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-rm -rf ./tcpdump
+set -e
+
+mkdir -p tcpdump logs
+rm -f tcpdump/*
+rm -f logs/*
 
 docker compose --profile "*" down
 docker compose --profile "*" build
 docker compose --profile nodebug up --force-recreate --abort-on-container-exit
+
+for service in $(docker compose ps -a --services); do
+    docker compose logs --no-log-prefix --no-color $service > logs/$service.log
+done
